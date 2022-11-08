feb48fe0529018b7489e022c332422b76d14a411
Himanshu Raj <himanshurajverma549@gmail.com>
2022-11-08 18:17:41 +0530

    removed test

--- scripts/scriptInjection.js
+++ scripts/scriptInjection.js
@@ -26,7 +26,7 @@ const scriptMap = {
         'all': ['queryforwarding.js', 'timer.js', 'animate-faq.js',],
     },
     staging: {
-        '^(\/)$': ['feature-dropdown.js', 'priceChange.js', 'slider.js', 'test.js'],
+        '^(\/)$': ['feature-dropdown.js', 'priceChange.js', 'slider.js',],
         // basic monthly page
         '(\/pro)$': ['slider.js'],
         // Basic annual page
